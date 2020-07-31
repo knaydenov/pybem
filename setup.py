70f1c5cfa692ef5c2e558090537e1a60e6097094
Klim Naydenov <knaydenov@gmail.com>
2020-07-31 12:55:44 +0300

    Set semver to 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pybem',
-    version='0.1',
+    version='0.1.0',
     description='This package provides helpers for BEM classes generation',
     url='https://github.com/knaydenov/pybem',
     install_requires=[],
